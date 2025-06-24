4d54337570aea9bfb3f6700f9208c8d8f5d22189
tornike thcalidze <ttchalidze@gmail.com>
2025-06-23 21:38:35 -0400

    fixed issue with imports

--- api/users.js
+++ api/users.js
@@ -1,9 +1,9 @@
+import express from "express";
 const router = express.Router();
 export default router;
-
-import { createUser, getUserByEmailAndPassword } from "db/queries/users";
-import requireBody from "middleware/requireBody";
-import { createToken } from "utils/jwt";
+import { createUser, getUserByEmailAndPassword } from "#db/queries/users";
+import requireBody from "#middleware/requireBody";
+import { createToken } from "#utils/jwt";
 
 router
   .route("/register")
