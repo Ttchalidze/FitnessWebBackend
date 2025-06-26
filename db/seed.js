4bb40bfc7c69c92ffdecc2ea836abb21b0318f4e
tornike thcalidze <ttchalidze@gmail.com>
2025-06-26 01:01:32 -0400

    correct youtube links

--- db/seed.js
+++ db/seed.js
@@ -13,31 +13,31 @@ async function seed() {
       name: "Chest",
       description:
         "Target Muscles: Upper & Lower Chest. Length: approx. 20mins. Equipment Needed: Dumbbells",
-      url: "https://www.youtube.com/watch?v=cd1CjwxA918",
+      url: "https://www.youtube.com/embed/cd1CjwxA918?si=gTyf51ow32c8gCvj",
     },
     {
       name: "Arms",
       description:
         "Target Muscles: Biceps, Triceps, Forearms. Length: approx. 20-25mins. Equipment Needed: Dumbbells & Adjustable Bench",
-      url: "https://www.youtube.com/watch?v=qiBDUtA5kPw",
+      url: "https://www.youtube.com/embed/qiBDUtA5kPw?si=UuppQXuiADtIGAQy",
     },
     {
       name: "Back",
       description:
         "Target Muscles: Upper & Lower Back. Length: approx. 20-25mins. Equipment Needed: Dumbbells & Adjustable Bench",
-      url: "https://www.youtube.com/watch?v=pZ1Xzrh_els",
+      url: "https://www.youtube.com/embed/pZ1Xzrh_els?si=72ve4ioyKJ9PQEGj",
     },
     {
       name: "Legs",
       description:
         "Target Muscles: Quads, Glutes, Hamstrings & Calves. Length: approx. 20-25mins. Equipment Needed: Dumbbells",
-      url: "https://www.youtube.com/watch?v=x5PAjQrG_UM",
+      url: "https://www.youtube.com/embed/x5PAjQrG_UM?si=qyo4maITyZPIcOu6",
     },
     {
       name: "Shoulders",
       description:
         "Target Muscles: Front, Side & Rear Delts/Shoulder. Length: approx. 20-25mins. Equipment Needed: Dumbbells & Adjustable Bench",
-      url: "https://www.youtube.com/watch?v=tn2vBcwjdm0",
+      url: "https://www.youtube.com/embed/tn2vBcwjdm0?si=8seX9gtHTDFyKf5e",
     },
   ];
 
